4a177e16925a5e3a0a109433aee02015ee4a665a
EmanuelCottarel <emanuelcottarel@gmail.com>
2024-07-25 16:08:17 +0200

    exo 1 a 7

--- src/test/java/java17/ex07/DateAndTime_07_Test.java
+++ src/test/java/java17/ex07/DateAndTime_07_Test.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertThat;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.ZoneId;
+import java.util.Calendar;
 import java.util.Date;
 
 import org.junit.Test;
@@ -18,13 +20,14 @@ public class DateAndTime_07_Test {
 	public void test_date_to_localdate() throws Exception {
 
 		// TODO Créer une date Java 1 (12/02/2017)
-		Date date = null;
+		Date date = new Date(117, Calendar.FEBRUARY,12);
 
 		// TODO transformer la date en Instant
+		date.toInstant();
 
 		// TODO transformer la date en LocalDate
 
-		LocalDate result = null;
+		LocalDate result = LocalDate.ofInstant(date.toInstant(), ZoneId.of("Europe/Paris"));
 
 		// TODO valoriser les différentes variables afin de rendre le test passant
 
